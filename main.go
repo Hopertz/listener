eb2f0b85f56951b44d3f9752a460d41fbec7fe3a
Hopertz <lugano.ngulwa@gmail.com>
2023-02-19 10:20:05 +0300

    Add simple webhook listener

--- main.go
+++ main.go
@@ -1,97 +1,64 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	"net/http"
-	"os"
-	"time"
 
+	"github.com/julienschmidt/httprouter"
 	"listener.hopertz.me/webhooks"
 )
 
-type Middleware func(next http.Handler) http.Handler
+func verifyWebhookHandler(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Query()
 
-// Wraps a http.Handler with a middlewares
-func Wrap(handler http.Handler, middlewares ...Middleware) http.Handler {
-	// wraps backwards
-	for i := len(middlewares) - 1; i >= 0; i-- {
-		handler = middlewares[i](handler)
+	mode := key.Get("hub.mode")
+	token := key.Get("hub.verify_token")
+	challenge := key.Get("hub.challenge")
+
+	if len(mode) > 0 && len(token) > 0 {
+		if mode == "subscribe" && token == "mytesttoken" {
+			w.WriteHeader(http.StatusOK)
+			resJson, _ := json.Marshal(challenge)
+			w.Write(resJson)
+			log.Println("Hurray")
+			return
+
+		} else {
+			w.WriteHeader(http.StatusForbidden)
+			log.Println("forbiden")
+			return
+		}
+
 	}
-	return handler
+	w.WriteHeader(http.StatusBadRequest)
+	log.Println("bad request")
+	return
 }
 
-// StartTimeMiddleware is a middleware that adds the start time to the request context
-func StartTimeMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("StartTimeMiddleware")
-		ctx := r.Context()
-		ctx = context.WithValue(ctx, "startTime", time.Now())
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
+func webHookEventHandler(w http.ResponseWriter, r *http.Request) {
+	var notification webhooks.Notification
+	defer r.Body.Close()
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&notification)
+	log.Println(notification)
 
-// EndTimeMiddleware is a middleware that adds the end time to the request context
-func EndTimeMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("EndTimeMiddleware")
-		ctx := r.Context()
-		ctx = context.WithValue(ctx, "endTime", time.Now())
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
 
-var _ webhooks.EventHandler = (*handler)(nil)
-
-type handler struct {
-	// it can be db conections, etc
-	// connection to notify the user
-	// etc etc
-}
-
-func (h *handler) HandleError(ctx context.Context, writer http.ResponseWriter, request *http.Request, err error) error {
 	if err != nil {
-		os.Stdout.WriteString(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		return
+	} else {
+		w.WriteHeader(http.StatusAccepted)
 	}
 
-	os.Stdout.WriteString("error is nil")
-	return nil
-}
-
-func (h *handler) HandleEvent(ctx context.Context, writer http.ResponseWriter, request *http.Request, notification *webhooks.Notification) error {
-	os.Stdout.WriteString("HandleEvent")
-	jsonb, err := json.Marshal(notification)
-	if err != nil {
-		return err
-	}
-	writer.WriteHeader(http.StatusOK)
-	writer.Header().Set("Content-Type", "application/json")
-	writer.Write(jsonb)
-	return nil
 }
 
 func main() {
-	// Create a new handler
-	handler := &handler{}
-	ls := webhooks.NewEventListener(handler)
-	middlewares := []Middleware{
-		StartTimeMiddleware,
-		EndTimeMiddleware,
-	}
-	finalHandler := Wrap(ls, middlewares...)
-	mux := http.NewServeMux()
-	mux.Handle("/webhooks", finalHandler)
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/webhooks", verifyWebhookHandler)
+	router.HandlerFunc(http.MethodPost, "/webhooks", webHookEventHandler)
 
-	// Create a new server
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
-
-	// Start the server
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
